4673dc9461ddf80d5a079c1d7cd47be2e2ed65e9
ano333333 <ano333333github@gmail.com>
2026-05-04 05:13:35 +0900

    refactor: モジュールの不要なエクスポートの削除

--- src/components/issue/mod.rs
+++ src/components/issue/mod.rs
@@ -3,5 +3,3 @@ pub mod journal;
 pub mod relative;
 
 pub use issue::IssueComponent;
-pub use journal::JournalComponent;
-pub use relative::RelativeIssueComponent;
--- src/components/mod.rs
+++ src/components/mod.rs
@@ -4,6 +4,3 @@ pub mod issue;
 
 pub use app::AppComponent;
 pub use base::Component;
-pub use issue::IssueComponent;
-pub use issue::JournalComponent;
-pub use issue::RelativeIssueComponent;
