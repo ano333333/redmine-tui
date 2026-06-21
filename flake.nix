b859787c93baefc92ce791adcad79e4445cac484
ano333333 <ano333333github@gmail.com>
2026-06-22 04:31:27 +0900

    chore: line coverageの追加

--- flake.nix
+++ flake.nix
@@ -11,9 +11,19 @@
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [ rustc cargo cargo-insta clippy ];
+          buildInputs = with pkgs; [
+            rustc
+            cargo
+            cargo-insta
+            cargo-llvm-cov
+            llvmPackages_latest.llvm
+            clippy
+          ];
 
           shellHook = ''
+            export LLVM_COV=$(command -v llvm-cov)
+            export LLVM_PROFDATA=$(command -v llvm-profdata)
+
             echo "Rust development environment loaded"
             echo "Rust version: $(rustc --version)"
             echo ""
