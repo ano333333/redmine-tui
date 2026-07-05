1d514462187abbc02d21681567a569533c65fe4b
ano333333 <ano333333github@gmail.com>
2026-07-06 01:48:01 +0900

    refactor: #[allow(unused_imports)]を外す

--- src/entities/mod.rs
+++ src/entities/mod.rs
@@ -14,7 +14,6 @@ pub use issue::Issue;
 pub use issue_status::IssueStatus;
 pub use journal::{Journal, JournalDetail, JournalDetailAttr};
 pub use priority::Priority;
-#[allow(unused_imports)]
 pub use project::Project;
 pub use tracker::Tracker;
 pub use user::User;
