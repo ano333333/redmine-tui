7ab47a5a131ed31f6e8497068e6e79de11eb158d
ano333333 <ano333333github@gmail.com>
2026-07-15 03:03:30 +0900

    refactor: body::Componentでキー解釈とアクション処理を分離

--- src/components/issue/body/component.rs
+++ src/components/issue/body/component.rs
@@ -19,6 +19,14 @@ pub enum EventProcessResult {
     EditRequested { id: u16, body: String },
 }
 
+enum Action {
+    MoveLeft,
+    MoveRight,
+    MoveDown,
+    MoveUp,
+    Edit,
+}
+
 pub struct BodyComponent {
     id: u16,
     width: u16,
@@ -43,52 +51,8 @@ impl BodyComponent {
     }
 
     pub fn process_event(&mut self, event: Event) -> Option<EventProcessResult> {
-        if let Some(cursor) = self.cursor_position
-            && let Event::Key(key) = event
-        {
-            match key.code {
-                KeyCode::Char('h') => {
-                    self.cursor_position = Some(Position {
-                        x: cursor.x.saturating_sub(1),
-                        y: cursor.y,
-                    });
-                }
-                KeyCode::Char('l') => {
-                    if cursor.x + 1 < self.width {
-                        self.cursor_position = Some(Position {
-                            x: cursor.x + 1,
-                            y: cursor.y,
-                        });
-                    }
-                }
-                KeyCode::Char('j') => {
-                    if cursor.y + 1 >= self.height {
-                        return Some(EventProcessResult::CursorLeavedFromBelow { x: cursor.x });
-                    }
-                    self.cursor_position = Some(Position {
-                        x: cursor.x,
-                        y: cursor.y + 1,
-                    });
-                }
-                KeyCode::Char('k') => {
-                    if cursor.y == 0 {
-                        return Some(EventProcessResult::CursorLeavedFromAbove { x: cursor.x });
-                    }
-                    self.cursor_position = Some(Position {
-                        x: cursor.x,
-                        y: cursor.y - 1,
-                    });
-                }
-                KeyCode::Char('e') => {
-                    return Some(EventProcessResult::EditRequested {
-                        id: self.id,
-                        body: self.body.clone(),
-                    });
-                }
-                _ => {}
-            }
-        }
-        None
+        let action = self.action_from_event(event)?;
+        self.apply_action(action)
     }
 
     pub fn focus_event(&mut self, event: FocusEvent) {
@@ -146,5 +110,69 @@ impl BodyComponent {
 
     pub fn get_cursor_position(&self) -> Position {
         self.cursor_position.unwrap_or(Position::new(0, 0))
+    }
+
+    fn action_from_event(&self, event: Event) -> Option<Action> {
+        let Event::Key(key) = event else {
+            return None;
+        };
+
+        self.cursor_position?;
+
+        match key.code {
+            KeyCode::Char('h') => Some(Action::MoveLeft),
+            KeyCode::Char('l') => Some(Action::MoveRight),
+            KeyCode::Char('j') => Some(Action::MoveDown),
+            KeyCode::Char('k') => Some(Action::MoveUp),
+            KeyCode::Char('e') => Some(Action::Edit),
+            _ => None,
+        }
+    }
+
+    fn apply_action(&mut self, action: Action) -> Option<EventProcessResult> {
+        let cursor = self.cursor_position?;
+
+        match action {
+            Action::MoveLeft => {
+                self.cursor_position = Some(Position {
+                    x: cursor.x.saturating_sub(1),
+                    y: cursor.y,
+                });
+                None
+            }
+            Action::MoveRight => {
+                if cursor.x + 1 < self.width {
+                    self.cursor_position = Some(Position {
+                        x: cursor.x + 1,
+                        y: cursor.y,
+                    });
+                }
+                None
+            }
+            Action::MoveDown => {
+                if cursor.y + 1 >= self.height {
+                    return Some(EventProcessResult::CursorLeavedFromBelow { x: cursor.x });
+                }
+                self.cursor_position = Some(Position {
+                    x: cursor.x,
+                    y: cursor.y + 1,
+                });
+                None
+            }
+            Action::MoveUp => {
+                if cursor.y == 0 {
+                    return Some(EventProcessResult::CursorLeavedFromAbove { x: cursor.x });
+                }
+                self.cursor_position = Some(Position {
+                    x: cursor.x,
+                    y: cursor.y - 1,
+                });
+                None
+            }
+            Action::Edit => Some(EventProcessResult::EditRequested {
+                id: self.id,
+                body: self.body.clone(),
+            }),
+        }
     }
 }
