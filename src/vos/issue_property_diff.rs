3bdad65b58d2c845bc46a3726c09a868635384ef
ano3 <ano333333github@gmail.com>
2026-07-27 04:54:23 +0900

    refactor: Issueの変更差分を持つVOの作成

---
+++ src/vos/issue_property_diff.rs
@@ -0,0 +1,147 @@
+use chrono::{DateTime, Local};
+
+use crate::vos::{IssueId, IssueStatusId, PriorityId, ProjectId, TrackerId, UserId};
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IssueSubjectDiff {
+    pub before: String,
+    pub after: String,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IssueAuthorIdDiff {
+    pub before: UserId,
+    pub after: UserId,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IssueCreatedOnDiff {
+    pub before: DateTime<Local>,
+    pub after: DateTime<Local>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IssueUpdatedOnDiff {
+    pub before: DateTime<Local>,
+    pub after: DateTime<Local>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IssueProjectIdDiff {
+    pub before: ProjectId,
+    pub after: ProjectId,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IssueTrackerIdDiff {
+    pub before: TrackerId,
+    pub after: TrackerId,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IssueStatusIdDiff {
+    pub before: IssueStatusId,
+    pub after: IssueStatusId,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IssuePriorityIdDiff {
+    pub before: PriorityId,
+    pub after: PriorityId,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IssueAssignedToIdDiff {
+    pub before: Option<UserId>,
+    pub after: Option<UserId>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IssueFixedVersionDiff {
+    pub before: Option<String>,
+    pub after: Option<String>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IssueStartDateDiff {
+    pub before: Option<DateTime<Local>>,
+    pub after: Option<DateTime<Local>>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IssueDueDateDiff {
+    pub before: Option<DateTime<Local>>,
+    pub after: Option<DateTime<Local>>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IssueDoneRatioDiff {
+    pub before: u16,
+    pub after: u16,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IssueEstimatedHoursDiff {
+    pub before: Option<u16>,
+    pub after: Option<u16>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IssueTotalSpentHoursDiff {
+    pub before: Option<f64>,
+    pub after: Option<f64>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IssueResolveWayDiff {
+    pub before: Option<String>,
+    pub after: Option<String>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IssueComponentDiff {
+    pub before: String,
+    pub after: String,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IssueDescriptionDiff {
+    pub before: String,
+    pub after: String,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IssueChildIdsDiff {
+    pub before: Vec<IssueId>,
+    pub after: Vec<IssueId>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IssueJournalIdsDiff {
+    pub before: Vec<u16>,
+    pub after: Vec<u16>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum IssuePropertyDiff {
+    Subject(IssueSubjectDiff),
+    AuthorId(IssueAuthorIdDiff),
+    CreatedOn(IssueCreatedOnDiff),
+    UpdatedOn(IssueUpdatedOnDiff),
+    ProjectId(IssueProjectIdDiff),
+    TrackerId(IssueTrackerIdDiff),
+    StatusId(IssueStatusIdDiff),
+    PriorityId(IssuePriorityIdDiff),
+    AssignedToId(IssueAssignedToIdDiff),
+    FixedVersion(IssueFixedVersionDiff),
+    StartDate(IssueStartDateDiff),
+    DueDate(IssueDueDateDiff),
+    DoneRatio(IssueDoneRatioDiff),
+    EstimatedHours(IssueEstimatedHoursDiff),
+    TotalSpentHours(IssueTotalSpentHoursDiff),
+    ResolveWay(IssueResolveWayDiff),
+    Component(IssueComponentDiff),
+    Description(IssueDescriptionDiff),
+    ChildIds(IssueChildIdsDiff),
+    JournalIds(IssueJournalIdsDiff),
+}
--- src/vos/mod.rs
+++ src/vos/mod.rs
@@ -1,8 +1,10 @@
 pub mod id;
+pub mod issue_property_diff;
 pub mod journal_detail;
 
 pub use id::{
     EntityId, EntityIdValue, IssueId, IssueStatusId, JournalId, PriorityId, ProjectId,
     TimeEntityActivityId, TrackerId, UserId,
 };
+pub use issue_property_diff::IssuePropertyDiff;
 pub use journal_detail::{JournalDetail, JournalDetailAttr};
