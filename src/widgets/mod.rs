4804c018bd49f60e5a4abf0412d55fc802499615
ano3 <ano333333github@gmail.com>
2026-09-20 16:01:43 +0900

    refactor: 未使用Widget(Hr)の削除

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -1,10 +1,8 @@
 pub mod gutter;
-pub mod hr;
 pub mod monthly_calendar_widget;
 pub mod theme;
 pub mod toast_widget;
 pub mod vertical_scroll_widget;
 
-pub use hr::Hr;
 pub use toast_widget::ToastWidget;
 pub use vertical_scroll_widget::{VerticalScrollWidget, VerticalScrollWidgetState};
